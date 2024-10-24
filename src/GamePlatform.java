089c293e62538a7dddb07fe8e6d0cd378c8130a6
yingjieho <yingjieho@outlook.sg>
2024-10-24 15:47:06 +0800

    Init Question 1 and 2

---
+++ src/GamePlatform.java
@@ -0,0 +1,8 @@
+public class GamePlatform {
+    public static double getFinalSpeed(double initialSpeed, int[] inclinations) {
+        throw new UnsupportedOperationException("Waiting to be implemented.");
+    }
+    public static void main(String[] args){
+        System.out.println(getFinalSpeed(60.0, new int[] { 0, -30, 0, 45, 0 })); //should print 45
+    }
+}
